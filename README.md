b185c4142d2649206d7b8e85db07c56a940a82e5
denev <65989857+deneb0723@users.noreply.github.com>
2021-01-03 05:40:42 +0900

    Setting Router part 1

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Jjapflix
+
+무비 디스커버리 앱 with React (feat. nomadcoders)
--- src/App.js
+++
@@ -1,7 +0,0 @@
-import React, { component } from "react";
-
-function App() {
-  return <div className="App"></div>;
-}
-
-export default App;
