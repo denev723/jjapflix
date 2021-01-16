b0b3726202c5d59f8723f3ccad0d2f9768cff8a4
denev <65989857+deneb0723@users.noreply.github.com>
2021-01-16 23:28:45 +0900

    detail container coding

--- src/api.js
+++ src/api.js
@@ -3,41 +3,41 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   params: {
-    api_key: "24e57914be24aef87dfd80b279f9bb90",
+    api_key: "10923b261ba94d897ac6b81148314a3f",
     language: "en-US",
   },
 });
 
-export const tvApi = {
-  topRated: () => api.get("tv/top_rated"),
-  popular: () => api.get("tv/popular"),
-  airingToday: () => api.get("tv/airing_today"),
+export const movieApi = {
+  nowPlaying: () => api.get("movie/now_playing"),
+  upcoming: () => api.get("movie/upcoming"),
+  popular: () => api.get("movie/popular"),
   movieDetail: (id) =>
     api.get(`movie/${id}`, {
       params: {
-        apeend_to_response: "videos",
+        append_to_response: "videos",
       },
     }),
   search: (term) =>
-    api.get("search/tv", {
+    api.get("search/movie", {
       params: {
         query: encodeURIComponent(term),
       },
     }),
 };
 
-export const movieApi = {
-  nowPlaying: () => api.get("movie/now_playing"),
-  upcoming: () => api.get("movie/upcoming"),
-  popular: () => api.get("movie/popular"),
+export const tvApi = {
+  topRated: () => api.get("tv/top_rated"),
+  popular: () => api.get("tv/popular"),
+  airingToday: () => api.get("tv/airing_today"),
   showDetail: (id) =>
     api.get(`tv/${id}`, {
       params: {
-        apeend_to_response: "videos",
+        append_to_response: "videos",
       },
     }),
   search: (term) =>
-    api.get("search/movie", {
+    api.get("search/tv", {
       params: {
         query: encodeURIComponent(term),
       },
